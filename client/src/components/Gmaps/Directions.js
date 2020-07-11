780cc75fa58c90be09c799559fc2a7a338d5dd19
Adam SOudani <asoudani@email.com>
2020-07-11 10:49:17 -0700

    distance matrix

--- client/src/components/Gmaps/Directions.js
+++ client/src/components/Gmaps/Directions.js
@@ -6,72 +6,118 @@ import {
   GoogleMap,
   DirectionsRenderer
 } from "react-google-maps";
-import DistanceDisplay from '../components/DistanceDisplay';
+import DistanceDisplay from '../DistanceDisplay';
+import axios from 'axios';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
+
 
 class Directions extends React.Component {
   state = {
-    directions: null
+    directions: null,
+    origin: { lat: 34.0522, lng: -118.2437 },
+    destination: { lat: 32.7157, lng: -117.1611 }
   };
 
-  // displayMarkers = () => {
-  //   return this.state.stores.map((store, index) => {
-  //     return <Marker key={index} id={index} position={{
-  //      lat: store.latitude,
-  //      lng: store.longitude
-  //    }}
-  //    onClick={() => console.log("You clicked me!")} />
-  //   })
+  // getDistance() {
+  //   const origin = this.state.origin;
+  //   const destination = this.state.destination;
+  //   const travelMode = google.maps.TravelMode.DRIVING;
+
+  //   axios.post('/api/distance', { origin, destination, travelMode })
+  //     .then(res => res.data)
+  //     .then(data => this.props.setDist(data))
+  //     .catch(err => console.log('Unable to get distances: ' + err))
   // }
+  findDistance = () => {
+    var service = new google.maps.DistanceMatrixService();
+    service.getDistanceMatrix({
+      origins: [this.state.origin],
+      destinations: [this.state.destination],
+      travelMode: 'DRIVING',
+      unitSystem: google.maps.UnitSystem.IMPERIAL
+    }, callback);
 
-  componentDidMount(){
-    const directionsService = new google.maps.DirectionsService();
-    const origin = { lat: 34.0522, lng: -118.2437 };
-    const destination = { lat: 32.7157, lng: -117.1611 };
+    function callback(response, status) {
+      if (status !== 'OK') {
+        alert('Error was: ' + status);
+      } else {
+        // console.log(response);
+        //response.rows[0].elements[0].distance.text
+        //response.rows[0].elements[0].duration.text
+        var origins = response.originAddresses;
+        var destinations = response.destinationAddresses;
 
-    directionsService.route(
-      {
-        origin: origin,
-        destination: destination,
-        travelMode: google.maps.TravelMode.DRIVING
-      },
-      (result, status) => {
-        if (status === google.maps.DirectionsStatus.OK) {
-          this.setState({
-            directions: result
-          });
-        } else {
-          console.error(`error fetching directions ${result}`);
+        for (var i = 0; i < origins.length; i++) {
+          var results = response.rows[i].elements;
+          for (var j = 0; j < results.length; j++) {
+            var element = results[j];
+            var distance = element.distance.text;
+            var duration = element.duration.text;
+            var from = origins[i];
+            var to = destinations[j];
+            console.log("Distance: " + distance);
+            console.log("Duration: " + duration);
+          }
+
         }
       }
-    );
-
+    }
   }
 
-  render() {
-    const GoogleMapExample = withGoogleMap(props => (
-      <GoogleMap
-        defaultCenter={{ lat: 33.4274, lng: -117.6126 }}
-        defaultZoom={13}
-      >
-        <DirectionsRenderer
-          directions={this.state.directions}
-        />
-      </GoogleMap>
-      
-    ));
-
-    return (
-      <div>
-        <GoogleMapExample
-          containerElement={<div style={{ height: `750px`, width: "75%" }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
-        <DistanceDisplay />
-      </div>
-    );
-  }
-}
 
 
 
-export default Directions;
+      componentDidMount(){
+        const directionsService = new google.maps.DirectionsService();
+        const origin = this.state.origin;
+        const destination = this.state.destination;
+
+        directionsService.route(
+          {
+            origin: origin,
+            destination: destination,
+            travelMode: google.maps.TravelMode.DRIVING
+          },
+          (result, status) => {
+            if (status === google.maps.DirectionsStatus.OK) {
+              this.setState({
+                directions: result
+              });
+              this.findDistance();
+            } else {
+              console.error(`error fetching directions ${result}`);
+            }
+          }
+        );
+
+      }
+
+      render() {
+        const GoogleMapExample = withGoogleMap(props => (
+          <GoogleMap
+            defaultCenter={{ lat: 33.4274, lng: -117.6126 }}
+            defaultZoom={13}
+          >
+            <DirectionsRenderer
+              directions={this.state.directions}
+            />
+          </GoogleMap>
+
+        ));
+
+        return (
+          <div>
+            <GoogleMapExample
+              containerElement={<div style={{ height: `750px`, width: "75%" }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+            />
+            {/* <DistanceDisplay /> */}
+          </div>
+        );
+      }
+    }
+
+
+
+    export default Directions;
--- client/src/components/OriginDest.js
+++ client/src/components/OriginDest.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react';
 
-export default class OriginDest extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            origin: '',
-            destination: ''
-        }
-        this.originRef = React.createRef();
-        this.destinationRef = React.createRef();
-    }
+// export default class OriginDest extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             origin: '',
+//             destination: ''
+//         }
+//         this.originRef = React.createRef();
+//         this.destinationRef = React.createRef();
+//     }
 
-    componentDidMount() {
-        this.originRef.current.focus;
-    }
+//     componentDidMount() {
+//         this.originRef.current.focus;
+//     }
 
-    clickHandler = () => {
-        this.setState({
-            origin: this.originRef.current.value,
-            destination: this.destinationRef.current.value
-        })
-    }
+//     clickHandler = () => {
+//         this.setState({
+//             origin: this.originRef.current.value,
+//             destination: this.destinationRef.current.value
+//         })
+//     }
 
-    render() {
-        return (
-            <div>
-                <input type='text'
-                    ref={this.originRef}
-                />
-                <input type='text'
-                    ref={this.destinationRef}
-                />
-                <button onClick={this.clickHandler}>Submit</button>
-            </div>
-        )
-    }
-}
+//     render() {
+//         return (
+//             <div>
+//                 <input type='text'
+//                     ref={this.originRef}
+//                 />
+//                 <input type='text'
+//                     ref={this.destinationRef}
+//                 />
+//                 <button onClick={this.clickHandler}>Submit</button>
+//             </div>
+//         )
+//     }
+// }
 
