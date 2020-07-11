2e487b3f2ee3dafdd505306e4d58f6b0bb86dea9
Yuko Uda <yuko.uda0110@gmail.com>
2020-07-11 12:34:03 -0700

    Fix proxy error 500

--- routes/API/index.js
+++ routes/API/index.js
@@ -1,15 +1,11 @@
 const router = require("express").Router();
+const path = require("path");
+// const googleRoutes = require("./googleMaps");
 const tripsRoutes = require("./trips");
+
 
 // Book routes
 router.use("/trips", tripsRoutes);
-const path = require("path");
-const socketio = require("./socketio");
-const googleRoutes = require("./googleMaps");
-
-
-// router.use("/socket", socketio);
-router.use("/distance", googleRoutes);
 
 router.use(function(req, res) {
     res.sendFile(path.join(__dirname, "../../client/build/index.html"));
