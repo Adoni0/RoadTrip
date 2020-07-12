e4f0e76ba4415acfb91e09a7676fd03d99e15a6d
Yuko Uda <yuko.uda0110@gmail.com>
2020-07-12 13:41:14 -0700

    Edit User model and Trip model, create seedDB for User Collection(table), and add 'npm run seed' command to the script

--- models/Trip.js
+++ models/Trip.js
@@ -4,29 +4,34 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var TripSchema = new Schema ({
-    tripName: {
-        type: String,
-        required: true
-      },
-      origin:{
-        type: String,
-        required: true
-      },
-      destination:{
-        type: String,
-        required: true
-      },
-      numberOfStops:{
-          type: Number,
-          required: true
-      },
-      //integer 1-3
-      budget:{
-        type: Number,
-        min: 1,
-        max:3,
-        required: true
-    }
+  tripName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  origin: {
+    type: String,
+    required: true
+  },
+  destination: {
+    type: String,
+    required: true
+  },
+  numberOfStops: {
+      type: Number,
+      required: true
+  },
+  //integer 1-3
+  budget: {
+    type: Number,
+    min: 1,
+    max:3,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 // This creates our model from the above schema, using mongoose's model method
