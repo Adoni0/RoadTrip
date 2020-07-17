ca68e21e5daed1d96712bc5744496c89d7ee198e
Adam SOudani <asoudani@email.com>
2020-07-16 21:11:42 -0700

    fix stopover points

--- client/src/components/Gmaps/index.js
+++ client/src/components/Gmaps/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { withScriptjs } from "react-google-maps";
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import Directions from './Directions';
 
 export class Gmaps extends Component {
@@ -16,6 +15,7 @@ export class Gmaps extends Component {
                 loadingElement={<div style={{ height: `100%` }} />}
                 inputOrigin={this.props.inputOrigin}
                 inputDestination={this.props.inputDestination}
+                stops={this.props.stops}
             />
 
         )
