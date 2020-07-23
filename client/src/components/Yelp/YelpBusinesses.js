913eefb0a7839947d9ddb86fba8f7e66d73d884c
Yuko Uda <yuko.uda0110@gmail.com>
2020-07-23 13:22:43 -0700

    Style YelpBusinesses

--- client/src/components/Yelp/YelpBusinesses.js
+++ client/src/components/Yelp/YelpBusinesses.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import API from '../../utils/API';
+import './style.css';
 
 class YelpBusinesses extends React.Component {
     state = {
@@ -76,29 +77,45 @@ class YelpBusinesses extends React.Component {
                 {this.state.stops.length === Object.keys(this.state.yelpBusinesses).length && this.state.stops.length >= 1 ?
                     this.state.stops.map((stop) => {
                         return (
-                            <div>
-                                <h2 className="text-capitalize text-center mb-4">Route stop: {stop}</h2>
+                            <div className="stop-container">
+                                <h2 className="heading-stops">
+                                    <span className="sub">
+                                        Route stop
+                                    </span>
+                                    <ion-icon name="pin"></ion-icon>
+                                    <span className="main">
+                                        {stop}
+                                    </span>
+                                </h2>
                                 <div className="row row-cols-1 row-cols-md-2">
                                     {!this.isYelpBusinessesEmpty(this.state.yelpBusinesses) ? this.state.yelpBusinesses[stop].map((business) => {
                                         console.log(business);
 
                                         return (
                                             <div className="col mb-4" style={this.cardStyle}>
-                                                <div className="card">
+                                                <div className="card business">
                                                     <img src={business.image_url} className="card-img-top max-width:100%" width="200" height="300" alt={business.name} />
                                                     <div className="card-body">
-                                                        <h5 className="card-title"><u>{business.name}</u></h5>
-                                                        <p className="card-text lead">Reviews: {business.review_count}</p>
-                                                        <p className="card-text lead">Rating: {business.rating} Stars</p>
-                                                        <p className="card-text lead">Phone #: {business.display_phone}</p>
-                                                        <p className="card-text lead"><a href={business.url} target="_blank" rel="noopener noreferrer" >Click here for more info...</a></p>
+                                                        <h3 className="business-name">
+                                                            <a href={business.url} target="_blank" rel="noopener noreferrer" className="link-stop">
+                                                                {business.name}
+                                                            </a>
+                                                        </h3>
+                                                        <p>Reviews: {business.review_count}</p>
+                                                        <p>Rating: {business.rating} Stars</p>
+                                                        <p>Phone #: {business.display_phone}</p>
+                                                        <p>
+                                                            <a href={business.url} target="_blank" rel="noopener noreferrer" className="link-stop">
+                                                                Click here for more info...
+                                                            </a>
+                                                        </p>
                                                     </div>
                                                 </div>
                                             </div>
                                         );
                                     }) : null}
                                 </div>
-                            </div >
+                            </div>
                         )
                     }) : null}
             </>
