da92302a0850a5df8ab5a33644345f78c50d8027
Adam SOudani <asoudani@email.com>
2020-07-29 15:41:20 -0700

    update readme with gifs

--- routes/API/googleMaps.js
+++
@@ -1,9 +0,0 @@
-var router = require('express').Router();
-const projectController = require('../../controllers/projectController');
-
-
-// routes to /api/distance
-router.route('/').get(projectController.findDistance);
-
-
-module.exports = router;
